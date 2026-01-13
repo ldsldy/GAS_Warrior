d0e3bfadef2783bd13534db8d8d12f3c8b987470
LDY <137361731+ldsldy@users.noreply.github.com>
2026-01-13 18:00:42 +0900

    커스텀 블루프린트 라이브러리 제작

---
+++ Source/Warrior/Public/WarriorFunctionLibrary.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WarriorFunctionLibrary.generated.h"
+
+class UWarriorAbilitySystemComponent;
+
+UENUM()
+enum class EWarriorConfirmType : uint8
+{
+	Yes,
+	No
+};
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	// C++ 전용: 액터로부터 워리어 어빌리티 시스템 컴포넌트를 가져옵니다.
+	static UWarriorAbilitySystemComponent* NativeGetWarriorASCFromActor(AActor* InActor);
+
+	// 액터에 지정된 게임플레이 태그가 없으면 추가합니다.
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary")
+	static void AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd);
+
+	// 액터에 지정된 게임플레이 태그가 있으면 제거합니다.
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary")
+	static void RemoveGameplayTagFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove);
+
+	// 액터가 지정된 게임플레이 태그를 가지고 있는지 확인합니다.
+	static bool NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck);
+
+	// 액터가 지정된 게임플레이 태그를 가지고 있는지 확인합니다. (블루프린트용, enum타입을 아웃풋으로 지정하여 여러개의 실행핀을 만든다.)
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary", meta = (DisplayName = "Does Actor Have Tag", ExpandEnumAsExecs = "OutConfirmType"))
+	static void BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, EWarriorConfirmType& OutConfirmType);
+};
