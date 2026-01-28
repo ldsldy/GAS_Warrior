21e3a88f5ad96f03ff096052ae37fe8b4a2c3517
LDY <137361731+ldsldy@users.noreply.github.com>
2026-01-28 11:57:10 +0900

    적의 공격이 실제 데미지를 처리

--- Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -2,13 +2,46 @@
 
 
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "WarriorGameplayTags.h"
 
 #include "WarriorDebugHealper.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	// 중복 충돌 방지
+	if (OverlappedActors.Contains(HitActor))
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" hit ") + HitActor->GetActorNameOrLabel());
+		return;
+	}
+
+	OverlappedActors.AddUnique(HitActor);
+
+	// TODO:: Implement block check
+	bool bIsValidBlock = false;
+
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnBlockable = false;
+
+	if (bIsPlayerBlocking && !bIsMyAttackUnBlockable)
+	{
+		// TODO:: check if the block is valid
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+
+	if (bIsValidBlock)
+	{
+		// TODO:: Handle successful block
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			WarriorGameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
 	}
 }
